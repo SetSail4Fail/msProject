8ab9aa9927dd8eb63756f705d831b51a5995a8d9
SetSail4Fail <jo.lepton@gmail.com>
2025-04-02 20:31:29 +0300

    new deposit func

--- internal/sql/operations/deposit.go
+++ internal/sql/operations/deposit.go
@@ -1,2 +1,24 @@
 package operations
 
+import (
+	"log"
+	"msProject/config"
+	"msProject/mypkg/postgres"
+
+	"github.com/google/uuid"
+)
+
+type DepositRequest struct {
+	Value int
+	Uuid  uuid.UUID
+}
+
+func deposit(cfg *config.Config, DepositRequest DepositRequest) {
+	InsertQuery := "UPDATE accounts SET balance = balance + $1 WHERE id = $2 VALUES ($1, $2)"
+	db := postgres.DbConnect(cfg)
+ 
+	_, err := db.Exec(InsertQuery, DepositRequest.Value, DepositRequest.Uuid)
+	if err != nil {
+		log.Fatalf("User not found", err)
+	}
+}
